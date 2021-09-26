1d75ba401e0c1638a3ddea396f2c8ae1da2553d0
Dias Kamza <68639981+kamzadias@users.noreply.github.com>
2021-09-26 22:39:19 +0600

    add CoinGeckoAPI.py

--- src/CoinGeckoAPI.py
+++ src/CoinGeckoAPI.py
@@ -1,1 +1,16 @@
+from pycoingecko import CoinGeckoAPI
+cg = CoinGeckoAPI()
 
+coinsMarket = cg.get_coins_markets(vs_currency = 'usd')
+
+x = 0
+n = int(input("Enter number of coins : "))
+
+coinsList = []
+for i in coinsMarket:
+    coinsList.append(i["name"])
+    if x == n-1:
+        break
+    x += 1
+
+print(coinsList)
